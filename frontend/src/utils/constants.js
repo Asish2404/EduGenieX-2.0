1bc3863b42c222d31069d1625fdae3b88258b3bd
Asish2404 <asishbose313@gmail.com>
2026-06-10 19:19:57 +0530

    render deployment

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,5 +1,5 @@
 // export const API_BASE_URL = "https://edugeniex-2-0.onrender.com/api";
-export const API_BASE_URL = "http://localhost:8000/api";
+export const API_BASE_URL = "https://edugeniex-2-0.onrender.com/api";
 export const featureCards = [
   {
     key: 'tutor',
